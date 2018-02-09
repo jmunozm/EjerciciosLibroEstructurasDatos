063ac674f2b8f238d3beb05d6842fe1f9b15179e
BRM05461H\BRM05461 <juan.munoz@banregio.com>
2018-02-08 18:43:10 -0600

    Commit a MaxTest y ThrowDemo

---
+++ src/c2/ThrowDemo.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package c2;
+
+import java.io.IOException;
+
+/**
+ * @author Juan Munoz
+ *
+ */
+public class ThrowDemo {
+	
+	public static void processFile(String toFile) throws IOException{
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for(String fileName : args){
+			
+		}
+	}
+
+}
