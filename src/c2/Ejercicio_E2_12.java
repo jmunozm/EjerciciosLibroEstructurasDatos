b15a0df43800fff06997f665120f5dc57c43f9c9
BRM05461H\BRM05461 <juan.munoz@banregio.com>
2018-10-29 11:03:38 -0600

    Actualizacion_ 29/Oct/2018 Configuré branch en pc del trabajo

---
+++ src/c2/Ejercicio_E2_12.java
@@ -0,0 +1,133 @@
+package c2;
+
+import java.util.Random;
+
+/**
+ * @author Juan Munoz
+ *
+ */
+public class Ejercicio_E2_12 {
+	
+	public static final int TAMANO_MAXIMO_ARREGLO = 10;
+	public static final int VALOR_MAXIMO_RANDOM = 10;
+	public static final int NUMERO_ARREGLOS = 1;
+	
+	public static void main(String[] args) {
+		System.out.println("Arreglos tamaño randoom valores randoom");
+
+		double [] arreglo1;
+		
+		for(int i = 1; i<=NUMERO_ARREGLOS; i++){
+			arreglo1 = new double[valorRandoom(TAMANO_MAXIMO_ARREGLO)];
+			arreglo1 = asignarValores(arreglo1);
+			
+			//Para pruebas
+			arreglo1 = new double[12]; //1, 1, 1, 4, 4, 5, 5, 5, 7, 8, 9, 9, 9
+			arreglo1[0] = 4;
+			arreglo1[1] = 4;
+			arreglo1[2] = 4;
+			arreglo1[3] = 4;
+			arreglo1[4] = 4;
+			arreglo1[5] = 4;
+			arreglo1[6] = 4;
+			arreglo1[7] = 4;
+			arreglo1[8] = 4;
+			arreglo1[8] = 4;
+			arreglo1[10] = 4;
+			arreglo1[11] = 4;
+			arreglo1[12] = 4;
+			imprimirArreglo(arreglo1);
+			System.out.println("SUM: " + sum(arreglo1));
+			System.out.println("MEDIA: " + average(arreglo1));
+			System.out.println("MODA: " + mode(arreglo1));
+			System.out.println();
+		}
+	}
+	
+	public static double mode(double [] arr){
+		double arrModa [] = new double[arr.length];
+		double [][]matriz = new double[arr.length][2];
+		boolean guarda = true;
+		for(int i = 0; i<arr.length; i++){
+			guarda = true;
+			for(int j = 0; j<matriz.length; j++){
+				if(matriz[j][0]==arr[i]){
+					matriz[j][1]++;
+					guarda = false;
+					break;
+				}
+			}
+			if(guarda){
+				matriz[i][0] = arr[i];
+				matriz[i][1]++;	
+			}
+		}
+		double max = 0;
+		for(int i = 0; i<matriz.length; i++){
+			System.out.println(matriz[i][0] +" "+ matriz[i][1]);
+			if(matriz[i][1]>max){
+				max=matriz[i][1];
+				i=0;
+			}
+		}
+		
+		return max;
+	}	
+	
+	public static double average(double [] arr){
+		return sum(arr) / arr.length;
+	}
+	
+	public static double sum(double [] arr){
+		double suma = 0.0;
+		for (double d : arr) {
+			suma+=d;
+		}
+		return suma;
+	}
+	
+	public static double[] asignarValores(double arreglo[]){
+		for(int i = 0; i<arreglo.length; i++){
+			//arreglo[i] = valorRandoom(VALOR_MAXIMO_RANDOM);
+			arreglo[i] = valorRandoom(VALOR_MAXIMO_RANDOM);
+		}
+		return arreglo;
+	}
+	
+	public static int valorRandoom(int valorMaximo){
+		//Generar los numeros
+		Random r = new Random();
+		return r.nextInt(valorMaximo)+1;
+	}
+	
+	public static double valorRandoom(){
+		//Generar los numeros
+		Random r = new Random();
+		return r.nextDouble();
+	}
+	
+	public static void imprimirArreglo(int arreglo[]){
+		System.out.print("{");
+		for(int i = 0; i<arreglo.length; i++){
+			System.out.print(arreglo[i]);	
+			if(i<arreglo.length-1){
+				System.out.print(", ");
+			}
+		}
+		System.out.print("}\n");
+	}
+	
+	public static void imprimirArreglo(double arreglo[]){
+		System.out.print("{");
+		for(int i = 0; i<arreglo.length; i++){
+			System.out.print(arreglo[i]);	
+			if(i<arreglo.length-1){
+				System.out.print(", ");
+			}
+		}
+		System.out.print("}\n");
+	}
+	
+}
+
+
